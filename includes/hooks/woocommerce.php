7b6b9dd18a339483fd35515880c9eee93f982c5a
Md Dalwar <dalwar9195@gmail.com>
2021-07-08 22:39:19 +0600

    Woocommerce billing address fields class changed

--- includes/hooks/woocommerce.php
+++ includes/hooks/woocommerce.php
@@ -63,3 +63,62 @@ function saimon_output_all_notices() {
 }
 remove_action( 'woocommerce_cart_is_empty', 'woocommerce_output_all_notices', 5 );
 add_action( 'woocommerce_cart_is_empty', 'saimon_output_all_notices', 5 );
+
+function saimon_woocommerce_billing_fields( $fields ){
+	// First Name
+	$fields['billing_first_name']['class'][] = 'form-group';
+	$fields['billing_first_name']['input_class'][] = 'form-control';
+	$fields['billing_first_name']['placeholder'] = 'Your First Name';
+
+	// Last Name
+	$fields['billing_last_name']['class'][] = 'form-group';
+	$fields['billing_last_name']['input_class'][] = 'form-control';
+	$fields['billing_last_name']['placeholder'] = 'Your Last Name';
+
+	// Company Name
+	$fields['billing_company']['class'][] = 'form-group';
+	$fields['billing_company']['input_class'][] = 'form-control';
+	$fields['billing_company']['placeholder'] = 'Enter Company Name';
+
+	// Country Selection
+	$fields['billing_country']['class'][] = 'form-group';
+	$fields['billing_country']['input_class'][] = 'form-control custom-select';
+
+	// Address Line 1
+	$fields['billing_address_1']['class'][] = 'form-group';
+	$fields['billing_address_1']['input_class'][] = 'form-control';
+	$fields['billing_address_1']['placeholder'] = 'Address Line 1';
+
+	// Address Line 2
+	$fields['billing_address_2']['class'][] = 'form-group';
+	$fields['billing_address_2']['input_class'][] = 'form-control';
+	$fields['billing_address_2']['placeholder'] = 'Address Line 2';
+
+	// City
+	$fields['billing_city']['class'][] = 'form-group';
+	$fields['billing_city']['input_class'][] = 'form-control';
+	$fields['billing_city']['placeholder'] = 'Address Line 2';
+
+
+	// State Selection
+	$fields['billing_state']['class'][] = 'form-group';
+	$fields['billing_state']['input_class'][] = 'form-control custom-select';
+
+	// Post Code
+	$fields['billing_postcode']['class'][] = 'form-group';
+	$fields['billing_postcode']['input_class'][] = 'form-control';
+	$fields['billing_postcode']['placeholder'] = 'Enter Post Code';
+
+	// Phone Number
+	$fields['billing_phone']['class'][] = 'form-group';
+	$fields['billing_phone']['input_class'][] = 'form-control';
+	$fields['billing_phone']['placeholder'] = 'Your Phone Number';
+
+	// Phone Number
+	$fields['billing_email']['class'][] = 'form-group';
+	$fields['billing_email']['input_class'][] = 'form-control';
+	$fields['billing_email']['placeholder'] = 'Enter Email Address';
+
+	return $fields;
+}
+add_filter( 'woocommerce_billing_fields', 'saimon_woocommerce_billing_fields' );
