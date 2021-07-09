3e0d2ae6e3f539db437528c6d4250637d51e9529
Md Dalwar <dalwar9195@gmail.com>
2021-07-09 23:16:50 +0600

    Navigation fallback updated

--- includes/functions/admin.php
+++ includes/functions/admin.php
@@ -5,11 +5,15 @@
 		exit( 'Direct access denied.' );
 	}
 	
-	if(!function_exists('primary_menu_fallback')){
+	if( ! function_exists( 'primary_menu_fallback' ) ){
 		function primary_menu_fallback(){
 			$url = admin_url('nav-menus.php');
-
-			echo sprintf('<ul class="%s"><li id="menu-item-2" class="menu-item-2 nav-item"><a href="' . $url . '" class="nav-link">%s</a></li></ul>', pm_class(), 'Create a Menu');
+			if( current_user_can( 'manage_options' ) ){
+				echo sprintf('<ul class="%s"><li id="menu-item-2" class="menu-item-2 nav-item"><a href="' . $url . '" class="nav-link">%s</a></li></ul>', pm_class(), 'Create a Menu');
+			}else{
+				echo sprintf('<ul class="%s"><li id="menu-item-2" class="menu-item-2 nav-item"></li></ul>', pm_class());
+			}
+			
 		}
 	}
 	
